e99de3da5b286fc1b8cce5792f9faba1f910fcc6
ArtOfCode- <owen.jenkins2903@gmail.com>
2019-06-07 21:50:15 +0100

    Add reference service & action params

--- api.js
+++ api.js
@@ -9,7 +9,12 @@ const methods = [
     "GetHistoricTimeLine", "GetNextDepartures", "GetNextDeparturesWithDetails", "GetServiceDetailsByRID", "QueryHistoricServices", "QueryServices"
 ];
 
+const referenceMethods = ["GetReasonCode", "GetReasonCodeList", "GetSourceInstanceNames", "GetStationList", "GetTOCList"];
+
 const apiBaseUrl = 'https://lite.realtime.nationalrail.co.uk/OpenLDBSVWS/ldbsv12.asmx';
+const apiActionBase = 'http://thalesgroup.com/RTTI/2012-01-13/ldbsv';
+const referenceBaseUrl = 'https://lite.realtime.nationalrail.co.uk/OpenLDBSVWS/ldbsvref.asmx';
+const refActionBase = 'http://thalesgroup.com/RTTI/2015-05-14/ldbsv_ref';
 
 const isObject = obj => obj === Object(obj);
 
@@ -19,6 +24,13 @@ module.exports = class NationalRailClient {
             const result = [];
             obj.forEach(x => {
                 if (isObject(x) || x instanceof Array) {
+                    if (x['attr'] && x['attr']['xmlns']) {
+                        delete x['attr']['xmlns'];
+                        if (Object.keys(x['attr']).length === 0) {
+                            delete x['attr'];
+                        }
+                    }
+
                     result.push(NationalRailClient.denamespacify(x));
                 }
                 else {
@@ -34,6 +46,13 @@ module.exports = class NationalRailClient {
                 const newName = splat[splat.length - 1];
                 if (isObject(obj[k]) || obj[k] instanceof Array) {
                     result[newName] = NationalRailClient.denamespacify(obj[k]);
+
+                    if (result[newName]['attr'] && result[newName]['attr']['xmlns']) {
+                        delete result[newName]['attr']['xmlns'];
+                        if (Object.keys(result[newName]['attr']).length === 0) {
+                            delete result[newName]['attr'];
+                        }
+                    }
                 }
                 else {
                     result[newName] = obj[k];
@@ -50,6 +69,12 @@ module.exports = class NationalRailClient {
             const chars = m.split('');
             const methodName = chars[0].toLowerCase() + chars.slice(1, chars.length).join('');
             this[methodName] = this.createRequester(m);
+        });
+
+        referenceMethods.forEach(m => {
+            const chars = m.split('');
+            const methodName = chars[0].toLowerCase() + chars.slice(1, chars.length).join('');
+            this[methodName] = this.createRequester(m, true);
         });
     }
 
@@ -69,7 +94,7 @@ module.exports = class NationalRailClient {
         }).end({ pretty: true });
     }
 
-    createRequester(method) {
+    createRequester(method, isReference = false) {
         const requestType = `ldb:${method}Request`;
         return async params => {
             const xmlParams = {};
@@ -80,20 +105,20 @@ module.exports = class NationalRailClient {
             const body = this.createSoapRequest({
                 [requestType]: xmlParams
             });
-            const response = await fetch(apiBaseUrl, {
+            const response = await fetch(isReference ? referenceBaseUrl : apiBaseUrl, {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/soap+xml',
+                    'Content-Type': `application/soap+xml;charset=UTF-8;action=${isReference ? refActionBase : apiActionBase}/${method}`,
                     'Accept-Encoding': 'gzip, deflate'
                 },
                 body
             });
 
             const raw = await response.text();
-            const data = xmlParser.parse(raw);
+            const data = xmlParser.parse(raw, { textNodeName: 'content', attributeNamePrefix: '', attrNodeName: 'attr', ignoreAttributes: false });
             const responseType = `${method}Response`;
             const bodyData = data['soap:Envelope']['soap:Body'][responseType];
-            return NationalRailClient.denamespacify(bodyData[Object.keys(bodyData)[0]]);
+            return NationalRailClient.denamespacify(bodyData[Object.keys(bodyData).filter(x => x !== 'attr')[0]]);
         };
     }
 };
